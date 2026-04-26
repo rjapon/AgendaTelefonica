cf1226891439745e5bc311d171e44a8a79fd335d
rjapon <richardjv666@gmail.com>
2026-04-25 23:45:53 -0500

    Commit

--- src/main/java/ec/edu/ups/agendatelefonica/Agendatelefonica.java
+++ src/main/java/ec/edu/ups/agendatelefonica/Agendatelefonica.java
@@ -6,58 +6,129 @@ import ec.edu.ups.agendatelefonica.clases.Telefono;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
-/**
- *
- * @author Asus
- */
+
 public class Agendatelefonica {
 
     public static void main(String[] args) {
-        
-        Date fechaDeNacimientoUno = new Date(2000, 0, 15);
-        Persona personaUno = new Persona("0123", "Juan Perez", fechaDeNacimientoUno);
-        OperadoraTelefonica operadoraUno = new OperadoraTelefonica(1, "Claro");
-        
-        Telefono telefonoUno = new Telefono(997654321, "Movil", personaUno, operadoraUno);
-        
-        //Imprimir el objeto telefono
-                
-        //System.out.println(telefonoUno);
-        
-        //Manejo de Listas en Java
-        
+
         Scanner entrada = new Scanner(System.in);
-        ArrayList<OperadoraTelefonica> operadoras = new ArrayList();
-        
-        String continuar = "N";
-        do{
-            System.out.println("Ingresa el codigo de la operadora: ");
-            int codigo = entrada.nextInt();
-            System.out.println("Ingrese el nombre de la operadora: ");
-            String nombre = entrada.next();
-            
-            OperadoraTelefonica operadora = new OperadoraTelefonica();
-            operadora.setCodigo(codigo);
-            operadora.setNombre(nombre);
-            operadoras.add(operadora);
-            
-            System.out.println("¿Desea ingresar otra operadora? (S/N) ");
-            continuar = entrada.next();
-        }while(continuar.equalsIgnoreCase("S"));
-            
-        for (OperadoraTelefonica operadorita : operadoras){
-            System.out.println(operadorita);
-        }
-        
-        /*
-            Menu
-            1. Registrar Contactos (Implementar)
-            2. Registrar Operadoras (Implementar)
-            3. Registrar Telefonos (No implementar)
-            4. Salir
-            
-            Subir a GitHub (En teams)
-        */
-        
+
+        ArrayList<Telefono> contactos = new ArrayList<>();
+        ArrayList<OperadoraTelefonica> operadoras = new ArrayList<>();
+
+        int selec;
+
+        do {
+            System.out.println("\n--Menu--");
+            System.out.println("1. Ingresar contactos");
+            System.out.println("2. Ingresar operadoras");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opcion: ");
+            selec = entrada.nextInt();
+
+            switch (selec) {
+
+                case 1:
+                    String continuar1;
+
+                    do {
+                        System.out.println("\n--- Registrar contacto ---");
+
+                        System.out.print("Ingrese el numero de telefono: ");
+                        int numeroTelefono = entrada.nextInt();
+
+                        System.out.print("Ingrese el tipo de telefono: ");
+                        String tipoTelefono = entrada.next();
+
+                        System.out.print("Ingrese la cedula de la persona: ");
+                        String cedula = entrada.next();
+
+                        System.out.print("Ingrese el nombre de la persona: ");
+                        String nombrePersona = entrada.next();
+
+                        System.out.print("Ingrese el anio de nacimiento: ");
+                        int anio = entrada.nextInt();
+
+                        System.out.print("Ingrese el mes de nacimiento: ");
+                        int mes = entrada.nextInt();
+
+                        System.out.print("Ingrese el dia de nacimiento: ");
+                        int dia = entrada.nextInt();
+
+                        Date fechaNacimiento = new Date(anio - 1900, mes - 1, dia);
+
+                        Persona persona = new Persona();
+                        persona.setCedula(cedula);
+                        persona.setNombre(nombrePersona);
+                        persona.setFechaNacimiento(fechaNacimiento);
+
+                        System.out.print("Ingrese el codigo de la operadora: ");
+                        int codigoOperadora = entrada.nextInt();
+
+                        System.out.print("Ingrese el nombre de la operadora: ");
+                        String nombreOperadora = entrada.next();
+
+                        OperadoraTelefonica operadora = new OperadoraTelefonica();
+                        operadora.setCodigo(codigoOperadora);
+                        operadora.setNombre(nombreOperadora);
+
+                        Telefono telefono = new Telefono();
+                        telefono.setNumero(numeroTelefono);
+                        telefono.setTipo(tipoTelefono);
+                        telefono.setUnaPersona(persona);
+                        telefono.setUnaOperadora(operadora);
+
+                        contactos.add(telefono);
+
+                        System.out.println("\nContacto registrado:");
+                        System.out.println(telefono);
+
+                        System.out.print("\n¿Desea ingresar otro contacto? (S/N): ");
+                        continuar1 = entrada.next();
+
+                    } while (continuar1.equalsIgnoreCase("S"));
+
+                    break;
+
+                case 2:
+                    String continuar2;
+
+                    do {
+                        System.out.println("\n--- Registrar operadora ---");
+
+                        System.out.print("Ingrese el codigo de la operadora: ");
+                        int codigo = entrada.nextInt();
+
+                        System.out.print("Ingrese el nombre de la operadora: ");
+                        String nombre = entrada.next();
+
+                        OperadoraTelefonica operadora = new OperadoraTelefonica();
+                        operadora.setCodigo(codigo);
+                        operadora.setNombre(nombre);
+
+                        operadoras.add(operadora);
+
+                        System.out.println("\nOperadora registrada:");
+                        System.out.println(operadora);
+
+                        System.out.print("\n¿Desea ingresar otra operadora? (S/N): ");
+                        continuar2 = entrada.next();
+
+                    } while (continuar2.equalsIgnoreCase("S"));
+
+                    break;
+
+                case 3:
+                    System.out.println("Salir");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+        } while (selec != 3);
+
+        entrada.close();
     }
 }
